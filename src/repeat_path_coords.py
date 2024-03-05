5364c014e6e9b29f9bfb968c6620b8bada11096d
jardeldyonisio <jardel.dyonisio@hotmail.com>
2024-03-05 15:47:31 -0300

    added code to repeat path coords and adjuted topics to work with amcl and shark mb cmd_vel

---
+++ src/repeat_path_coords.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import time
+import rospy
+import numpy as np
+
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, PoseStamped
+from tf.transformations import euler_from_quaternion
+
+class Navigator:
+    THRESHOLD_YAW = .1
+    THRESHOLD_DIST = .1
+
+    def __init__(self):    
+        self.cmd_vel_pub = rospy.Publisher('/hoverboard_velocity_controller/cmd_vel', Twist, queue_size=10)
+        self.position_sub = rospy.Subscriber('/amcl_pose', Odometry, self.odom_callback, queue_size=10)
+
+        self.read_points_from_file('/home/fbotathome/fbot_ws/src/shark-mb-ros/data/teleop_data.txt')
+
+    def read_points_from_file(self, file_path):
+        self.goals = np.empty((0, 2))
+        with open(file_path, 'r') as f:
+            for line in f:
+                x, y = line.strip().split(',')
+                point = np.array([[float(x), float(y)]])
+                self.goals = np.append(self.goals, point, axis = 0)
+        return
+
+    def odom_callback(self, data: Odometry):
+        msg = Twist()
+
+        if len(self.goals) == 0:
+            msg.angular.z = 0.0
+            msg.linear.x = 0.0
+            return
+
+        quaternion = data.pose.pose.orientation 
+        quaternion_list = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+        _, _, self.yaw = euler_from_quaternion(quaternion_list)
+
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+
+        self.pos = np.array([self.x, self.y])
+
+        goal = self.goals[0]
+        goal_vec = np.array(goal)
+        self.dist_vec = goal_vec - self.pos
+        self.dist = np.linalg.norm(self.dist_vec)
+
+        self.yaw_d = np.arctan2(self.dist_vec[1], self.dist_vec[0])
+        self.diff_yaw = self.yaw_d - self.yaw
+
+        print("Goals", self.goals)
+    
+        if self.diff_yaw > self.THRESHOLD_YAW:
+            msg.linear.x = .0
+            msg.angular.z = .1
+        elif self.diff_yaw < -self.THRESHOLD_YAW:
+            msg.linear.x = .0
+            msg.angular.z = -.1
+        else:
+            msg.linear.x = .1
+            msg.angular.z = .0
+
+        if self.dist < self.THRESHOLD_DIST:
+            rospy.loginfo("Atingiu o objetivo")
+            self.goals = np.delete(self.goals, 0, 0)
+            self.cmd_vel_pub.publish(msg)
+            rospy.loginfo(self.goals)
+            if len(self.goals) == 0:
+                rospy.loginfo("Objetivos concluídos")
+                msg.angular.z = .0
+                msg.linear.x = .0
+        
+        self.cmd_vel_pub.publish(msg)
+
+if __name__ == '__main__':
+    rospy.init_node('bug1')
+    Navigator()
+    rospy.spin()
